35c27844ef10e26bf634e7d7d6ce56011bbc233f
SebastianDevus <sebastianjogosextra@gmail.com>
2022-04-02 01:46:18 -0300

    De agora em diante, testando positividade, negatividade ou neutralidade.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int numeroDigitado;
+
+Console.WriteLine();
+
+Console.Write("Digite um número: ");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+if (numeroDigitado < 0)
+{
+    Console.WriteLine("O número é negativo.");
+}
+else if (numeroDigitado > 0)
+{
+    Console.WriteLine("O número é positivo.");
+}
+else
+{
+    Console.WriteLine("O número é zero.");
+}
+
+Console.WriteLine();
